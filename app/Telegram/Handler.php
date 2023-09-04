f08bba781262e23da7f65309163ae32e72a81666
javlonnnn91 <javlonnnn91@gmail.com>
2023-09-04 15:23:56 +0500

    1

---
+++ app/Telegram/Handler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Telegram;
+
+use DefStudio\Telegraph\Facades\Telegraph;
+use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\ReplyButton;
+use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
+use Illuminate\Support\Stringable;
+
+class Handler extends WebhookHandler
+{
+    public function hello(): void
+    {
+        $this->reply('gagagagaag');
+    }
+
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        $this->reply('Bunday komanda yoq');
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        $this->reply($text);
+    }
+
+    public function start(): void
+    {
+
+        Telegraph::dice()->send();
+    }
+}
---
+++ routes/console.php
@@ -0,0 +1,28 @@
+<?php
+
+use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
+use DefStudio\Telegraph\Models\TelegraphBot;
+use DefStudio\Telegraph\Telegraph;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use DefStudio\Telegraph\Models\TelegraphChat;
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Artisan::command('tester', function () {
+
+    $chat = TelegraphChat::find(2);
+    dd($chat->message('sadasdsad'));
+});
