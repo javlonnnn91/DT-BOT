d6754523a80b4ed7a2d9418c63f35e3d80f32bce
javlonnnn91 <javlonnnn91@gmail.com>
2023-09-06 12:10:33 +0500

    tg bot

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -32,7 +32,7 @@ class Handler extends WebhookHandler
     public function start(): void
     {
         $message = 'Salom! Bizning Botga hush helibsiz! Xizmat ko‘rsatish tilini tanlang \n\n Привет! Добро пожаловать в наш бот! Выберите язык обслуживания';
-        $this->chat->message($message)
+        $this->chat->html($message)
             ->replyKeyboard(ReplyKeyboard::make()
                 ->row([
                     ReplyButton::make('Send Contact')->requestContact(),
